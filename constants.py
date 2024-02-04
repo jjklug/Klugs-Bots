af1b27f69b0861e593b952f34117b668e960f8f9
unknown <jackklug13@gmail.com>
2024-02-04 17:29:08 -0500

    class heirarchy

---
+++ constants.py
@@ -0,0 +1,10 @@
+import numpy
+
+#motor variables
+backleg_amplitude = numpy.pi/2
+backleg_frequency = 20
+backleg_phaseOffset = 0
+
+frontleg_amplitude = numpy.pi/4
+frontleg_frequency = 0
+frontleg_phaseOffset = 0
