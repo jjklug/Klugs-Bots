145bbbc95abbaf3fe3ff011a9dfdf76c7e71de3d
unknown <jackklug13@gmail.com>
2024-02-11 15:27:58 -0500

    Synapses done!

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -73,12 +73,10 @@ class NEURON:
     def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0)
 
-        print(self.Get_Value())
         for key in synapses:
             if key[1] == self.Get_Name():
                 self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[key].Get_Weight(), neurons[key[0]].Get_Value())
-        print(self.Get_Value())
-        exit()
+        self.Threshold()
 
     def Allow_Presynaptic_Neuron_To_Influence_Me(self, synapse_weight, presynaptic_neuron):
         result = synapse_weight*presynaptic_neuron
